369b200a324260d3b08fed34a1acb61410853435
Carlos Máximo <CarlosM@3sharp.com>
2020-08-21 18:10:55 -0700

    Updating local storage

--- BlazingPizza.ComponentsLibrary/wwwroot/localstorage.js
+++ BlazingPizza.ComponentsLibrary/wwwroot/localstorage.js
@@ -1,15 +1,14 @@
-﻿
-    blazorLocalStorage = {
-        set: (key, value) => {
-            localStorage.setItem(key, JSON.stringify(value));
-        },
-        get: key => {
-            return (key in localStorage ? JSON.parse(localStorage.getItem(key)) : null)
-        },
-        delete: key => {
-            localStorage.removeItem(key);
-        },
-        contains: key => {
-            return !(localStorage[key] === undefined);
-        }
+﻿blazorLocalStorage = {
+    set: (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    get: key => {
+        return (key in localStorage ? JSON.parse(localStorage.getItem(key)) : null)
+    },
+    delete: key => {
+        localStorage.removeItem(key);
+    },
+    contains: key => {
+        return !(localStorage[key] === undefined);
     }
+}
