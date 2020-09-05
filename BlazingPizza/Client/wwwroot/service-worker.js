ef8dcf0c382928c228e5571054d4867423fb9273
Carlos Máximo <CarlosM@3sharp.com>
2020-09-04 21:12:10 -0700

    Updating manifest

--- BlazingPizza/Client/wwwroot/service-worker.js
+++ BlazingPizza/Client/wwwroot/service-worker.js
@@ -3,5 +3,8 @@
     self.skipWaiting();  
 });
 
-
- 
+self.addEventListener('fetch', event => {
+    // You can add custom logic here for controlling whether to use cached data if offline, etc.
+    // The following line opts out, so requests go directly to the network as usual.
+    return null;
+});
